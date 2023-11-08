ae56101518567af211f134c79401c6011d94cb59
Domingojr21 <129601835+Domingojr21@users.noreply.github.com>
2023-11-08 12:49:02 -0400

    vamos a ver

---
+++ NetBanking/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetBanking.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+    }
+}
