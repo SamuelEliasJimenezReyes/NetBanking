15b5dc7f965bcc516fb1b89dd81cd2d55c20bec5
Domingojr21 <129601835+Domingojr21@users.noreply.github.com>
2023-11-16 12:19:52 -0400

    modficaciones para que beneficiary funcione pero simplemente agregue las propiedades a los modelos, hay que darle la logica correspondiente

--- NetBanking.Core.Application/ViewModel/Beneficiary/BeneficiaryVM.cs
+++ NetBanking.Core.Application/ViewModel/Beneficiary/BeneficiaryVM.cs
@@ -1,8 +1,12 @@
-﻿
-
-namespace NetBanking.Core.Application.ViewModel.Beneficiary
+﻿namespace NetBanking.Core.Application.ViewModel.Beneficiary
 {
     public class BeneficiaryVM
     {
+        public string IdentifyingNumberofProduct { get; set; } = null!;
+        public string BeneficiaryUserName { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public int Id { get; set; }
     }
 }
