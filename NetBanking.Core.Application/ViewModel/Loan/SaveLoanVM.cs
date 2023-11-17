5188b5bd65dabb59856b72c50c4f7101591327ef
Eliott Reyes <eliottreyes22@gmail.com>
2023-11-17 02:05:15 -0400

    En esta parte terminamos el editar productos de la parte de administrador, en la cual trbaajamos con las credit cards y los prestamos, para los dicionarios lo aplicamos en el repositorio de su respectiva entidad

--- NetBanking.Core.Application/ViewModel/Loan/SaveLoanVM.cs
+++ NetBanking.Core.Application/ViewModel/Loan/SaveLoanVM.cs
@@ -1,14 +1,18 @@
 ﻿using NetBanking.Core.Application.Dtos.User;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace NetBanking.Core.Application.ViewModel.Loan
 {
     public class SaveLoanVM
     {
+        public int Id {get; set;}
+        public string? IdentifyingNumber { get; set; } = null!;
+        public string UserNameofOwner { get; set; } = null!;
+
+        public string? UserName {get; set; } = null!;
+        public decimal LoanQuantity { get; set; }
+        public decimal? PaidQuantity { get; set; }
+
         public List<UserDTO>? users;
     }
 }
