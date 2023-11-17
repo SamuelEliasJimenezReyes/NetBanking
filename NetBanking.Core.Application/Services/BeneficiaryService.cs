3e3d25e937bb5d4784083fd346c3059c7dd9e537
Samuel Jimenez <samuelelias13@hotmail.com>
2023-11-17 01:32:30 -0400

    Beneficiario 20%

--- NetBanking.Core.Application/Services/BeneficiaryService.cs
+++ NetBanking.Core.Application/Services/BeneficiaryService.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using NetBanking.Core.Application.Dtos.Account;
+using NetBanking.Core.Application.Helpers;
 using NetBanking.Core.Application.Interfaces.Repositories;
 using NetBanking.Core.Application.Interfaces.Services;
 using NetBanking.Core.Application.ViewModel.Beneficiary;
@@ -14,19 +17,32 @@ namespace NetBanking.Core.Application.Services
     public class BeneficiaryService : GenericService<SaveBeneficiaryVM, BeneficiaryVM, Beneficiary>, IBeneficiaryService
     {
         private readonly IBeneficiaryRepository _beneficiaryRepository;
+        private readonly  ISavingAccountService _savingAccountService;
+        private readonly AuthenticationResponse userSession;
+        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IMapper _mapper;
 
-        public BeneficiaryService(IBeneficiaryRepository beneficiaryRepository, IMapper mapper) : base(beneficiaryRepository, mapper)
+        public BeneficiaryService(IBeneficiaryRepository beneficiaryRepository, IMapper mapper, ISavingAccountService savingAccountService, IHttpContextAccessor httpContextAccessor) : base(beneficiaryRepository, mapper)
         {
             _beneficiaryRepository = beneficiaryRepository;
             _mapper = mapper;
+            _savingAccountService = savingAccountService;
+            _httpContextAccessor = httpContextAccessor;
+            userSession = _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user");
         }
 
         public async Task<bool> AddBeneficiary(string identifyingNumber)
-        {
-            
+        { 
             var addedSuccessfully = await _beneficiaryRepository.AddBeneficiary(identifyingNumber);
             return addedSuccessfully;
+        }
+
+        public override async Task<SaveBeneficiaryVM> Add(SaveBeneficiaryVM vm)
+        {
+            var savingAccounts = await _savingAccountService.GetAllViewModel();
+
+            savingAccounts.Where(x =>x.IdentifyingNumber == vm.IdentifyingNumberofProduct);
+            return base.Add(vm);
         }
     }
 }
--- NetBanking.Core.Application/ViewModel/Beneficiary/SaveBeneficiaryVM.cs
+++ NetBanking.Core.Application/ViewModel/Beneficiary/SaveBeneficiaryVM.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace NetBanking.Core.Application.ViewModel.Beneficiary
 {
     public class SaveBeneficiaryVM
     {
+        public string? BeneficiaryUserName { get; set; } 
+        public string? UserName { get; set; } 
+
         [Required(ErrorMessage = "El número de cuenta es requerido")]
         [RegularExpression(@"^\d{9}$", ErrorMessage = "El número de cuenta debe tener 9 dígitos")]
         public string IdentifyingNumberofProduct { get; set; }
