25c7fe3c90c6ffaf58d3a6cc73d5bf075227eb66
Domingojr21 <129601835+Domingojr21@users.noreply.github.com>
2023-11-18 17:41:51 -0400

    METODO DE DASHBOARD READY

--- NetBanking.Core.Application/Services/ProductServices.cs
+++ NetBanking.Core.Application/Services/ProductServices.cs
@@ -22,18 +22,58 @@ namespace NetBanking.Core.Application.Services
         private readonly IMapper _mapper;
         private readonly AuthenticationResponse userSession;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly IUserService _userService;
 
         public ProductServices(ICreditCardService creditCardService, 
             ISavingAccountService savingAccountService, 
             ILoanService loanService, IMapper mapper, 
-            IHttpContextAccessor httpContextAccessor)
+            IHttpContextAccessor httpContextAccessor,
+             ITransactionRepository transactionRepository,
+             IUserService userService)
         {
+            _transactionRepository = transactionRepository;
             _creditCardService = creditCardService;
             _savingAccountService = savingAccountService;
             _loanService = loanService;
             _mapper = mapper;
             _httpContextAccessor = httpContextAccessor;
             userSession = _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user");
+            _userService = userService;
+        }
+
+        public async Task<DashBoardStatitics> GetDashBoard()
+        {
+            var credictCardList = await _creditCardService.GetAllViewModel();
+            var savingAccountList = await _savingAccountService.GetAllViewModel();
+            var loanList = await _loanService.GetAllViewModel();
+            var transactionsList = await _transactionRepository.GetAllAsync();
+            var dayTransactions = transactionsList.Where(x => x.Date == DateTime.Now).ToList();
+            var paymentList = transactionsList.Where(x => x.TransactionTypeId == 1 && x.TransactionTypeId == 2 && x.TransactionTypeId == 3 && x.TransactionTypeId == 4).ToList();
+            var dayPayment = paymentList.Where(x => x.Date == DateTime.Now).ToList();
+            var userList = await _userService.GetAllUsers();
+            var clientList = userList.Where(x => x.Roles.Contains("Client")).ToList();
+            var activeClient = clientList.Where(x => x.IsActive == true).ToList();
+            var inActiveClient = clientList.Where(x => x.IsActive == false).ToList();
+
+
+            var dashBoardStatitics = new DashBoardStatitics
+            {
+
+                TotalTransactions = transactionsList.Count,
+                DayTransactions = dayTransactions.Count,
+                TotalPayments = paymentList.Count,
+                DayPayments = dayPayment.Count,
+                TotalActiveClients = activeClient.Count,
+                TotalInactiveClients = inActiveClient.Count,
+                TotalCreditCards = credictCardList.Count,
+                TotalSavingAccounts = savingAccountList.Count,
+                TotalLoans = loanList.Count,
+                TotalProducts = credictCardList.Count + savingAccountList.Count + loanList.Count
+           };
+
+            return dashBoardStatitics;
+
         }
       
         public async Task<ProductVM> GetAllProducts()
---
+++ NetBanking.Core.Application/ViewModel/Products/DashBoardStatitics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBanking.Core.Application.ViewModel.Products
+{
+    public class DashBoardStatitics
+    {
+        public int TotalTransactions { get; set; }
+        public int DayTransactions { get; set; }
+        public int TotalPayments { get; set; }
+        public int DayPayments { get; set;}
+        public int TotalActiveClients { get; set; }
+        public int TotalInactiveClients { get; set; }
+        public int TotalCreditCards { get; set; }
+        public int TotalSavingAccounts { get; set; }
+        public int TotalLoans { get; set; }
+        public int TotalProducts { get; set; }
+    }
+}
